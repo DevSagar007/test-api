8e554dfe4125b9a43cab486f059159451476c349
DevSagar007 <sagorkhan9294@gmail.com>
2025-08-19 00:49:46 +0600

    start new project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,17 +22,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
         {children}
       </body>
     </html>
